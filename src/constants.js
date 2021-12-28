435258295829680b8f90015639b0bf1124a5a255
Siu <siuc@protonmail.com>
2021-12-28 18:00:15 -0500

    remake reducer

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,12 @@ export const PAUSE = "PAUSE";
 export const UPDATE = "UPDATE";
 export const SETTINGS = "SETTINGS";
 export const RESET = "RESET";
+
+export const defaultState = {
+  sLength: 25,
+  bLength: 5,
+  session: true,
+  second: 25 * 60,
+  paused: true,
+  endTime: 0,
+};
--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,37 +1,97 @@
 import { useReducer } from "react";
-import { START, PAUSE, UPDATE, SETTINGS, RESET } from "../constants";
+import {
+  START,
+  PAUSE,
+  UPDATE,
+  SETTINGS,
+  RESET,
+  defaultState,
+} from "../constants";
 
 const useData = () => {
   const reducers = {
-    [START]: (state, action) => {
-      return { ...state, paused: false };
+    [START]: (state, { timeNow }) => {
+      const endTime = timeNow + state.second;
+      return { ...state, paused: false, endTime };
     },
-    [PAUSE]: (state, action) => {
-      return { ...state };
+    [PAUSE]: (state, { timeNow }) => {
+      const second = state.entTime - timeNow;
+      return { ...state, paused: true, second };
     },
-    [UPDATE]: (state, action) => {
-      return { ...state };
+    [UPDATE]: (state, { timeNow }) => {
+      let { second, session, bLength, sLength, endTime } = state;
+      if (state.paused) {
+        const timeLeft = state.endTime - timeNow;
+        if (timeLeft > 0) {
+          second = timeLeft;
+          if (timeLeft < 1) {
+            let audio = document.getElementById("beep");
+            if (audio.pause) {
+              audio.currentTime = 0;
+              audio.play();
+            }
+          }
+        }
+      }
+      if (!state.paused) {
+        let length = 0;
+        if (session) {
+          length = bLength;
+        } else {
+          length = sLength;
+        }
+        second = length * 60;
+        session = !session;
+        endTime = timeNow + second;
+      }
+      return { ...state, second, session, endTime };
     },
     [SETTINGS]: (state, action) => {
-      return { ...state };
+      let { sLength, bLength, session, second } = state;
+      switch (true) {
+        case action.session && action.increment:
+          if (sLength < 60) {
+            sLength++;
+          }
+          break;
+        case action.session && !action.increment:
+          if (sLength > 1) {
+            sLength--;
+          }
+          break;
+        case !action.session && action.increment:
+          if (bLength < 60) {
+            bLength++;
+          }
+          break;
+        case !action.session && !action.increment:
+          if (bLength > 1) {
+            bLength--;
+          }
+          break;
+      }
+      if (action.session === session) {
+        if (session) {
+          second = sLength * 60;
+        } else {
+          second = bLength * 60;
+        }
+      }
+      return { ...state, sLength, bLength, session, second };
     },
-    [RESET]: (state, action) => {
-      return { ...state };
+    [RESET]: () => {
+      let audio = document.getElementById("beep");
+      audio.pause();
+      audio.currentTime = 0;
+      return defaultState;
     },
   };
 
   const reducer = (state, action) => {
     return reducers[action.type](state, action) || state;
   };
-  
-  const [state, dispatch] = useReducer(reducer, {
-    sLength: 25,
-    bLength: 5,
-    session: true,
-    second: 25 * 60,
-    paused: true,
-    endTime: 0,
-  });
+
+  const [state, dispatch] = useReducer(reducer, defaultState);
 };
 
 export default useData;
