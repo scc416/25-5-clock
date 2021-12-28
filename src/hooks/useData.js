3fc46e6ea74031a552e372be36b9a56bf2051282
Siu <siuc@protonmail.com>
2021-12-28 17:50:12 -0500

    adding info to reducer

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,30 +1,37 @@
 import { useReducer } from "react";
-import { START,PAUSE, UPDATE, SETTINGS, RESET } from "../constants";
+import { START, PAUSE, UPDATE, SETTINGS, RESET } from "../constants";
 
 const useData = () => {
   const reducers = {
-    // [UPDATE_VALUE]: (state, { value, calculations }) => {
-    //   return { value, calculations, enter: false };
-    // },
-    // [CLEAR]: () => {
-    //   return { value: "0", calculations: [], enter: false };
-    // },
-    // [ENTER]: (state, { calculations, value }) => {
-    //   return { value, calculations, enter: true };
-    // },
+    [START]: (state, action) => {
+      return { ...state, paused: false };
+    },
+    [PAUSE]: (state, action) => {
+      return { ...state };
+    },
+    [UPDATE]: (state, action) => {
+      return { ...state };
+    },
+    [SETTINGS]: (state, action) => {
+      return { ...state };
+    },
+    [RESET]: (state, action) => {
+      return { ...state };
+    },
   };
 
   const reducer = (state, action) => {
     return reducers[action.type](state, action) || state;
   };
+  
   const [state, dispatch] = useReducer(reducer, {
     sLength: 25,
     bLength: 5,
     session: true,
     second: 25 * 60,
     paused: true,
-    endTime: 0
+    endTime: 0,
   });
-}
+};
 
 export default useData;
