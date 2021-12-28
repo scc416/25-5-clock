892f525f5808121e66634e1e9e719cded82addef
Siu <siuc@protonmail.com>
2021-12-28 17:31:02 -0500

    remake layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,91 @@
 function App() {
   return (
-    <div>
-      Hello world
+    <div className="container column">
+      <div id="timer-label">
+        {/* {displaySession(this.props.session)} */}
+      </div>
+      <div id="time-left" className="timer">
+        {/* {displayTime(this.props.second)} */}
+      </div>
+      <div className="container main-controls">
+        <div
+          id="start_stop"
+          onClick={() =>console.log("stop")
+            // this.props.togglePaused(this.props.paused, Date.now() / 1000)
+          }
+        >
+          Start/Stop
+        </div>
+        <div id="reset" onClick={() => console.log("RESET")
+          // this.props.reset()
+        }
+          >
+          Reset
+        </div>
+      </div>
+      <div className="container">
+        <div className="sub-controls">
+          <div id="session-label" className="length-text">
+            Session Length
+          </div>
+          <div className="container">
+            <div className="number" id="session-length">
+              {/* {this.props.sLength} */}
+            </div>
+            <div className="container column arrows">
+              <div
+                id="session-increment"
+                onClick={() => console.log("INCREMENT")
+                  // this.props.setting(this.props.paused, true, true)
+                }
+              >
+                <i class="fas fa-arrow-circle-up"></i>
+              </div>
+              <div
+                id="session-decrement"
+                className="arrow-background"
+                onClick={() => console.log("INCREMENT")
+                  // this.props.setting(this.props.paused, true, false)
+                }
+              >
+                <i class="fas fa-arrow-circle-down"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="sub-controls">
+          <div id="break-label" className="length-text">
+            Break Length
+          </div>
+          <div className="container">
+            <div id="break-length" className="number">
+              {/* {this.props.bLength} */}
+            </div>
+            <div className="container column arrows">
+              <div
+                id="break-increment"
+                onClick={() => console.log("INCREMENT")
+                  // this.props.setting(this.props.paused, false, true)
+                }
+              >
+                <i class="fas fa-arrow-circle-up"></i>
+              </div>
+              <div
+                id="break-decrement"
+                onClick={() => console.log("INCREMENT")
+                  // this.props.setting(this.props.paused, false, false)
+                }
+              >
+                <i class="fas fa-arrow-circle-down"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <audio
+        id="beep"
+        src="https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-two/household_alarm_clock_beep_tone.mp3"
+      />
     </div>
   );
 }
