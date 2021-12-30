c86cc07ec412e2de74b3428ce7b4f482b6842234
Siu <siuc@protonmail.com>
2021-12-29 20:26:08 -0500

    separate subcontrol from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import useData from "./hooks/useData";
-import {
-  BREAK_DECREASE,
-  BREAK_INCREASE,
-  SESSION_DECREASE,
-  SESSION_INCREASE,
-} from "./constants";
+
 import MainControl from "./components/MainControl";
 import Audio from "./components/Audio";
+import SubControl from "./components/SubControl";
 
 const App = () => {
   const props = useData();
@@ -14,48 +10,7 @@ const App = () => {
   return (
     <>
       <MainControl {...props} />
-      <div className="container">
-        <div className="sub-controls">
-          <span>Session Length</span>
-          <div className="container">
-            <span>{sessionLength}</span>
-            <div className="container column arrows">
-              <div
-                className="clickable"
-                onClick={() => setting(SESSION_INCREASE, true)}
-              >
-                <i className="fas fa-arrow-circle-up"></i>
-              </div>
-              <div
-                className="arrow-background clickable"
-                onClick={() => setting(SESSION_DECREASE, true)}
-              >
-                <i className="fas fa-arrow-circle-down"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="sub-controls">
-          <span>Break Length</span>
-          <div className="container">
-            <span>{breakLength}</span>
-            <div className="container column arrows">
-              <div
-                className="clickable"
-                onClick={() => setting(BREAK_INCREASE, false)}
-              >
-                <i className="fas fa-arrow-circle-up"></i>
-              </div>
-              <div
-                id="clickable"
-                onClick={() => setting(BREAK_DECREASE, false)}
-              >
-                <i className="fas fa-arrow-circle-down"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SubControl {...{ sessionLength, breakLength, setting }} />
       <Audio />
     </>
   );
---
+++ src/components/SubControl.jsx
@@ -0,0 +1,52 @@
+import {
+  BREAK_DECREASE,
+  BREAK_INCREASE,
+  SESSION_DECREASE,
+  SESSION_INCREASE,
+} from "../constants";
+
+const SubControl = ({ sessionLength, breakLength, setting }) => {
+  return (
+    <div className="container">
+      <div className="sub-controls">
+        <span>Session Length</span>
+        <div className="container">
+          <span>{sessionLength}</span>
+          <div className="container column arrows">
+            <div
+              className="clickable"
+              onClick={() => setting(SESSION_INCREASE, true)}
+            >
+              <i className="fas fa-arrow-circle-up"></i>
+            </div>
+            <div
+              className="arrow-background clickable"
+              onClick={() => setting(SESSION_DECREASE, true)}
+            >
+              <i className="fas fa-arrow-circle-down"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="sub-controls">
+        <span>Break Length</span>
+        <div className="container">
+          <span>{breakLength}</span>
+          <div className="container column arrows">
+            <div
+              className="clickable"
+              onClick={() => setting(BREAK_INCREASE, false)}
+            >
+              <i className="fas fa-arrow-circle-up"></i>
+            </div>
+            <div id="clickable" onClick={() => setting(BREAK_DECREASE, false)}>
+              <i className="fas fa-arrow-circle-down"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubControl;
