1e29c9e68a369bf5742719d91b53e47370d980e2
Siu <siuc@protonmail.com>
2021-12-29 20:34:47 -0500

    build first subcontrol item

--- src/components/SubControlList.jsx
+++ src/components/SubControlList.jsx
@@ -5,29 +5,19 @@ import {
   SESSION_INCREASE,
 } from "../constants";
 
+import SubControlListItem from "./SubControlListItem";
+
 const SubControlList = ({ sessionLength, breakLength, setting }) => {
   return (
     <div className="container">
-      <div className="sub-controls">
-        <span>Session Length</span>
-        <div className="container">
-          <span>{sessionLength}</span>
-          <div className="container column arrows">
-            <div
-              className="clickable"
-              onClick={() => setting(SESSION_INCREASE, true)}
-            >
-              <i className="fas fa-arrow-circle-up"></i>
-            </div>
-            <div
-              className="arrow-background clickable"
-              onClick={() => setting(SESSION_DECREASE, true)}
-            >
-              <i className="fas fa-arrow-circle-down"></i>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SubControlListItem
+        {...{
+          setting,
+          name: "Session Length",
+          length: sessionLength,
+          session: true,
+        }}
+      />
       <div className="sub-controls">
         <span>Break Length</span>
         <div className="container">
--- src/components/SubControlListItem.jsx
+++ src/components/SubControlListItem.jsx
@@ -1,22 +1,37 @@
-const SubControlListItem = () => {
-  <div className="sub-controls">
-    <span>Session Length</span>
-    <div className="container">
-      <span>{sessionLength}</span>
-      <div className="container column arrows">
-        <div
-          className="clickable"
-          onClick={() => setting(SESSION_INCREASE, true)}
-        >
-          <i className="fas fa-arrow-circle-up"></i>
-        </div>
-        <div
-          className="arrow-background clickable"
-          onClick={() => setting(SESSION_DECREASE, true)}
-        >
-          <i className="fas fa-arrow-circle-down"></i>
+import {
+  BREAK_DECREASE,
+  BREAK_INCREASE,
+  SESSION_DECREASE,
+  SESSION_INCREASE,
+} from "../constants";
+
+const SubControlListItem = ({ name, setting, length, session }) => {
+  return (
+    <div className="sub-controls">
+      <span>{name}</span>
+      <div className="container">
+        <span>{length}</span>
+        <div className="container column arrows">
+          <div
+            className="clickable"
+            onClick={() =>
+              setting(session ? SESSION_INCREASE : BREAK_INCREASE, session)
+            }
+          >
+            <i className="fas fa-arrow-circle-up"></i>
+          </div>
+          <div
+            className="arrow-background clickable"
+            onClick={() =>
+              setting(session ? SESSION_DECREASE : BREAK_DECREASE, session)
+            }
+          >
+            <i className="fas fa-arrow-circle-down"></i>
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 };
+
+export default SubControlListItem;
