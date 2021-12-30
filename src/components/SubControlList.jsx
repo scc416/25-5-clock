7749721ac84641c82efbc0cf1300a37ded189621
Siu <siuc@protonmail.com>
2021-12-29 20:35:54 -0500

    build second sub control item

--- src/components/SubControlList.jsx
+++ src/components/SubControlList.jsx
@@ -1,10 +1,3 @@
-import {
-  BREAK_DECREASE,
-  BREAK_INCREASE,
-  SESSION_DECREASE,
-  SESSION_INCREASE,
-} from "../constants";
-
 import SubControlListItem from "./SubControlListItem";
 
 const SubControlList = ({ sessionLength, breakLength, setting }) => {
@@ -18,23 +11,14 @@ const SubControlList = ({ sessionLength, breakLength, setting }) => {
           session: true,
         }}
       />
-      <div className="sub-controls">
-        <span>Break Length</span>
-        <div className="container">
-          <span>{breakLength}</span>
-          <div className="container column arrows">
-            <div
-              className="clickable"
-              onClick={() => setting(BREAK_INCREASE, false)}
-            >
-              <i className="fas fa-arrow-circle-up"></i>
-            </div>
-            <div id="clickable" onClick={() => setting(BREAK_DECREASE, false)}>
-              <i className="fas fa-arrow-circle-down"></i>
-            </div>
-          </div>
-        </div>
-      </div>
+      <SubControlListItem
+        {...{
+          setting,
+          name: "Break Length",
+          length: breakLength,
+          session: false,
+        }}
+      />
     </div>
   );
 };
