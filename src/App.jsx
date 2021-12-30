8c44d613765cdc78002bf9ca56c09a664d25e74c
Siu <siuc@protonmail.com>
2021-12-29 20:06:28 -0500

    put audio into separated file

--- src/App.jsx
+++ src/App.jsx
@@ -83,10 +83,7 @@ const App = () => {
           </div>
         </div>
       </div>
-      <audio
-        id="beep"
-        src="https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-two/household_alarm_clock_beep_tone.mp3"
-      />
+      <Audio />
     </>
   );
 };
---
+++ src/components/Audio.jsx
@@ -0,0 +1,10 @@
+const Audio = () => {
+  return (
+    <audio
+      id="beep"
+      src="https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-two/household_alarm_clock_beep_tone.mp3"
+    />
+  );
+};
+
+export default Audio;
