7ca8f454a8a8572be64f5fe3d91e95b0c97b08db
Siu <siuc@protonmail.com>
2021-12-29 20:43:40 -0500

    seperating

--- src/components/SubControlListItemDisplay.jsx
+++ src/components/SubControlListItemDisplay.jsx
@@ -1,32 +1,10 @@
-import {
-  BREAK_DECREASE,
-  BREAK_INCREASE,
-  SESSION_DECREASE,
-  SESSION_INCREASE,
-} from "../constants";
+import SubContolListItemPanel from "./SubControlListItemPanel";
 
 const SubControlListItemDisplay = ({ setting, length, session }) => {
   return (
     <div className="container">
       <span>{length}</span>
-      <div className="container column arrows">
-        <div
-          className="clickable"
-          onClick={() =>
-            setting(session ? SESSION_INCREASE : BREAK_INCREASE, session)
-          }
-        >
-          <i className="fas fa-arrow-circle-up"></i>
-        </div>
-        <div
-          className="arrow-background clickable"
-          onClick={() =>
-            setting(session ? SESSION_DECREASE : BREAK_DECREASE, session)
-          }
-        >
-          <i className="fas fa-arrow-circle-down"></i>
-        </div>
-      </div>
+      <SubContolListItemPanel {...{ setting, session }} />
     </div>
   );
 };
